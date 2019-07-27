82c4499e3e31e0804bec31fd18ff21d8ebe22639
Joerg Behrmann <behrmann@physik.fu-berlin.de>
2019-07-27 19:13:39 +0200

    add Recipient View

--- resotool/resoapp/urls.py
+++ resotool/resoapp/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.ResolutionListView.as_view(), name="index"),
     path("<int:pk>/", views.ResolutionView.as_view(), name="resolution"),
     path("sendreso/<int:reso_id>/", views.send_resolution, name="send_reso"),
+    path("recipients/", views.RecipientListView.as_view(), name="recipients"),
 ]
--- resotool/resoapp/views.py
+++ resotool/resoapp/views.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest
 from django.shortcuts import render
 from django.views import generic
 
-from .models import Resolution
+from .models import Resolution, Recipient, RecipientCollection
 
 
 class ResolutionListView(generic.ListView):
@@ -16,3 +16,12 @@ class ResolutionView(generic.DetailView):
 
 def send_resolution(request, reso_id):
     return HttpRequest("works")
+
+
+class RecipientListView(generic.ListView):
+    def get_queryset(self):
+        return Recipient.objects.all()#.order_by("-name")
+
+
+class RecipientView(generic.DetailView):
+    model = Recipient
